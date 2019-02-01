1ae4143fd73c6b33735a35b41465c43ed6b9c731
danielbiagi <danielbiagi@gmail.com>
2019-02-01 16:52:36 -0200

    Update package.js

--- package.js
+++ package.js
@@ -18,9 +18,10 @@ promise1.then(function(value) {
   console.log(value);
   // expected output: "foo"
 });*/
-function adicionaMao(mao, seq, novaMao, exemplo, descricao) 
+function adicionaMao(seq, novaMao, exemplo, descricao) 
 {
-	var re = mao.concat("<tr><td>", seq, "</td>");
+	var re = "";
+	re = re.concat("<tr><td>", seq, "</td>");
 	re = re.concat("<td>", novaMao, "</td>");
 	re = re.concat("<td>", exemplo, "</td>");
 	re = re.concat("<td>", descricao, "</td></tr>");
@@ -39,16 +40,16 @@ fs.readFile('./index.html', function (err, html) {
      http.createServer(function (req, res) {
 	res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
         res.write(html);  
-	maos = adicionaMao(maos, "#1", "Royal Flush", "10 ♥  J ♥  Q ♥  K ♥  A ♥", "melhor mão do poker!");
-	maos = adicionaMao(maos, "#2", "Straight Flush", "4 ♣  5 ♣  6 ♣  7 ♣  8 ♣", "5 cartas na sequência, todas do msm naipe.");
-	maos = adicionaMao(maos, "#3", "Four of a Kind", "7 ♦  7 ♣  7 ♥  7 ♠  10 ♥", "4 cartas iguais de cada naipe.");
-        maos = adicionaMao(maos, "#4", "Full House", "K ♦  K ♥  K ♠  J ♥  J ♦", "Um par + uma trinca.");
-	maos = adicionaMao(maos, "#5", "Flush", "K ♠  7 ♠  6 ♠  2 ♠  A ♠", "5 cartas do msm naipe.");
-	maos = adicionaMao(maos, "#6", "Straight", "10 ♦  9 ♠  8 ♥  7 ♠  6 ♠", "5 cartas na sequência numérica.");
-	maos = adicionaMao(maos, "#7", "Three of a Kind", "K ♦  K ♥  K ♣  J ♥  2 ♣", "Trinca");
-	maos = adicionaMao(maos, "#8", "Two Pair", "A ♥  A ♠  4 ♦  4 ♥  Q ♦", "Dois pares");
-	maos = adicionaMao(maos, "#9", "One Pair", "5 ♠  5 ♣  Q ♥  8 ♣  7 ♣", "Um par");	     
-	maos = adicionaMao(maos, "#10", "High Card", "K ♥  Q ♦  7 ♦  3 ♣  2 ♥", "Carta alta");	
+	maos = adicionaMao("#1", "Royal Flush", "10 ♥  J ♥  Q ♥  K ♥  A ♥", "melhor mão do poker!");
+	maos = adicionaMao("#2", "Straight Flush", "4 ♣  5 ♣  6 ♣  7 ♣  8 ♣", "5 cartas na sequência, todas do msm naipe.");
+	maos = adicionaMao("#3", "Four of a Kind", "7 ♦  7 ♣  7 ♥  7 ♠  10 ♥", "4 cartas iguais de cada naipe.");
+        maos = adicionaMao("#4", "Full House", "K ♦  K ♥  K ♠  J ♥  J ♦", "Um par + uma trinca.");
+	maos = adicionaMao("#5", "Flush", "K ♠  7 ♠  6 ♠  2 ♠  A ♠", "5 cartas do msm naipe.");
+	maos = adicionaMao(#6", "Straight", "10 ♦  9 ♠  8 ♥  7 ♠  6 ♠", "5 cartas na sequência numérica.");
+	maos = adicionaMao("#7", "Three of a Kind", "K ♦  K ♥  K ♣  J ♥  2 ♣", "Trinca");
+	maos = adicionaMao("#8", "Two Pair", "A ♥  A ♠  4 ♦  4 ♥  Q ♦", "Dois pares");
+	maos = adicionaMao("#9", "One Pair", "5 ♠  5 ♣  Q ♥  8 ♣  7 ♣", "Um par");	     
+	maos = adicionaMao("#10", "High Card", "K ♥  Q ♦  7 ♦  3 ♣  2 ♥", "Carta alta");	
 	var r = maos.concat("</body>", "</html>");
 	res.end(r);
 	
